f6c5a720818798bae74ed483c24aa5784a6d4f7c
slawa-popow <110kwt@gmail.com>
2023-09-21 09:29:39 +0300

    фронт-енд всё. слив на верселя для тестов #2

--- public/src/settings.ts
+++ public/src/settings.ts
@@ -1,9 +1,8 @@
 
 export const settings = (() => {
 
-    const HOST = '/';
-
-    // const HOST = '';
+    // const HOST = '/';
+    const HOST = 'https://plitochka-quiz.vercel.app/';
 
     return {
         HOST
