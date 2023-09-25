1498b65fd0e6b010172fb6ae796eb0bddd489d02
slawa-popow <110kwt@gmail.com>
2023-09-25 12:38:31 +0300

    adddjjjj

--- src/routes/MainController.ts
+++ src/routes/MainController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { QuizeSendData } from '../types/appT';
 import { telegram } from './telegram';
 import { db } from '..';
-import { gdoc } from '../utils/gsheets';
+// import { gdoc } from '../utils/gsheets';
 // import { funcs } from '../utils/funcs';
 
 
@@ -58,7 +58,8 @@ class MainController {
             const res = await db.writeQuizData(data);
             if (Array.isArray(res) && res.length > 0) {
                 const d = await telegram.tgMessage(res[0]);
-                await gdoc(res[0], d);
+                console.log(d)
+                // await gdoc(res[0], d);
                 request.session.clientData = null;
                 request.session.save();
                 
