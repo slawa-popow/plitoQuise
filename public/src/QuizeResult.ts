d73b57cd3213b583acaa327838b30bcace761811
slawa-popow <110kwt@gmail.com>
2023-09-26 14:54:20 +0300

    session problem #5 redirect after send form #4

--- public/src/QuizeResult.ts
+++ public/src/QuizeResult.ts
@@ -75,12 +75,13 @@ export class QuizeResult {
                     this.sendErrMessage(Error.OK, result.status);
                     const idQuiz = result.rowId;
                     try {
+                        window.location.href = settings.HOST;
                         window.Telegram.WebApp.ready();
                         window.Telegram.WebApp.sendData(idQuiz);
                         window.Telegram.WebApp.close();
                         
                     } catch (e) {}
-                    window.location.href = settings.HOST;
+                    
                 } 
                 else if (result.status === 'redirect') {
                     window.location.href = settings.HOST;
