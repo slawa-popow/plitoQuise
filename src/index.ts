022b713aa644cb29a684ddc1af17e3fa1c353359
slawa-popow <110kwt@gmail.com>
2023-09-27 16:52:51 +0300

    refresh#39

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,8 @@ const secret = process.env.SECRET || '123';
 export const mysqlc = new MysqlClient();
 export const db = new Db(mysqlc);
 
-app.use(cors({credentials: true, origin: 'https://plitochka-quiz.vercel.app'}));
+app.use(cors({credentials: true, origin: ['https://plitochka-quiz.vercel.app', 
+    'https://plitochka-quiz.vercel.app/startquize', 'https://plitochka-quiz.vercel.app/sendquizdata']}));
 
 app.use(express.static(path.join(__dirname, '../public'))); 
 app.engine('handlebars', engine());
@@ -37,7 +38,9 @@ app.set('views', __dirname + '/../views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); 
- 
+
+const expiryOffset = 1*24*60*60*1000; // +1 day
+
 app.use(session({
   name: 'sos',
 	secret: secret,
@@ -45,7 +48,7 @@ app.use(session({
 	store: mysqlc.sessionStore,
   saveUninitialized: false,
 	resave: false,
-  cookie: {maxAge: 600000, sameSite: 'none', httpOnly: true, }
+  cookie: {expires: new Date(Date.now() + expiryOffset), secure: true, sameSite: 'none', httpOnly: true, }
 }));
 
 
