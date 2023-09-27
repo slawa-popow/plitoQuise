dbd2555f9eda420015260d5463e3c90e5fe4a70b
slawa-popow <110kwt@gmail.com>
2023-09-27 20:17:37 +0300

    статус 304 удалял cookie #5

--- public/index.ts
+++ public/index.ts
@@ -13,10 +13,9 @@ const btnStart = document.getElementById('btn_start_calc');
 btnStart?.addEventListener('click', async() => { 
     const url = 'https://plitochka-quiz.vercel.app/startquize';  
     await fetch(url, {
-        method: 'get',
         credentials: 'include',  
         headers: {
-            'Content-Type': 'application/json;charset=utf-8'
+            'Content-Type': 'application/json;charset=utf-8;'
           },
     });  
     // window.location.href = '/startquize';
