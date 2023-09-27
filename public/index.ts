aa2c32edfe4e7b5af0fbdac1767f2fe88da1c1f7
slawa-popow <110kwt@gmail.com>
2023-09-27 20:35:00 +0300

    статус 304 удалял cookie #6

--- public/index.ts
+++ public/index.ts
@@ -11,13 +11,8 @@ window.Telegram.WebApp.expand();
 
 const btnStart = document.getElementById('btn_start_calc');
 btnStart?.addEventListener('click', async() => { 
-    const url = 'https://plitochka-quiz.vercel.app/startquize';  
-    await fetch(url, {
-        credentials: 'include',  
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8;'
-          },
-    });  
-    // window.location.href = '/startquize';
+    // const url = 'https://plitochka-quiz.vercel.app/startquize';  
+   
+    window.location.href = '/startquize';
     
 });
