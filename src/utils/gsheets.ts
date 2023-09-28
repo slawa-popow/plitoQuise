7e59f7f8559bc0ccdb937761956e77091df8accd
slawa-popow <110kwt@gmail.com>
2023-09-28 10:36:40 +0300

    #90

--- src/utils/gsheets.ts
+++ src/utils/gsheets.ts
@@ -25,11 +25,7 @@ export async function gdoc(idrow: string, d: QuizeSendData) {
         
         await doc.loadInfo();
         const sheet = doc.sheetsByTitle['Квиз от Славяна'];
-
-        const arrWg = d.width_gates.split('/');
-        const arrWsg = d.width_second_gates.split('/');
-        const [wg, wsg] = formatGates(arrWg, arrWsg);
-
+        
         await sheet.addRow({ 
             dbrow_id: idrow,
             clients_id: d.clients_id || '',
@@ -49,31 +45,14 @@ export async function gdoc(idrow: string, d: QuizeSendData) {
             width_wicket: d.width_wicket, 
             width_second_wicket: d.width_second_wicket,
             how_many_gates: d.how_many_gates,
-            width_gates: wg,
-            width_second_gates: wsg,
+            width_gates: d.width_gates,
+            width_second_gates: d.width_second_gates,
             telegram: d.telegram || '',
             city: d.city,
             date: d.date || '',
             country: ''
         });
-    } catch (e) { console.log('err gdoc()');return}
+    } catch (e) { console.log(e);return}
     
       
-}
-
-function formatGates(wg: string[], wsg: string[]): string[] {
-    // откатные: 0/распашные: 4,5
-    const awg = wg.map(v => {
-        return (parseFloat(v) > 0) ? v : null;
-    })
-    .filter(fv => {
-        return (fv) ? fv : null;
-    });
-    const awsg = wsg.map(w => {
-        return (parseFloat(w) > 0) ? w : null;
-    }) .filter(fw => {
-        return (fw) ? fw : null;
-    });
-    
-    return [awg.join(' '), awsg.join(' ')]
 }
